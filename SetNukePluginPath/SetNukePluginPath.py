81d99ac857ca1087427251892a54a224dd5bf3ea
jasonhuang1115 <jasonhuang1115@users.noreply.github.com>
2018-07-23 16:51:51 -0700

    sync from Deadline repo

--- SetNukePluginPath/SetNukePluginPath.py
+++ SetNukePluginPath/SetNukePluginPath.py
@@ -12,8 +12,6 @@ from Deadline.Scripting import *
 
 ###############################################################
 #  Give Deadline an instance of this class so it can use it.
-#  If you've dug around the repository, you'll notice I poached
-#  this from our Draft code.
 ###############################################################
 def GetDeadlineEventListener():
     return SetNukePluginPathListener()
@@ -23,28 +21,30 @@ def CleanupDeadlineEventListener(eventListener):
 
 
 ###############################################################
-#  The Draft event listener class.
+#  The SetNukePluginPath event listener class.
 ###############################################################
 class SetNukePluginPathListener (DeadlineEventListener):
     def __init__(self):
         self.OnJobSubmittedCallback += self.OnJobSubmitted
-        self.OnJobStartedCallback += self.OnJobStarted
-    
+        # self.OnJobStartedCallback += self.OnJobStarted
         
-    def OnJobStarted(self, job):
-        self.CustomPluginDirectory='//lax04rend/Nikola/library/scripts/nuke'
-        # eventType = self.GetConfigEntryWithDefault( "SetNukePluginPathProperty", "On Job Started" )
-
-        # if 'Nuke' in job.PluginName and ( eventType == "On Job Started" or eventType == "On Job Started and On Job Finished" ):
-        #     self.CustomPluginDirectory='//lax04rend/Nikola/library/scripts/nuke'
-        #     #self.RemoveFiles( job )
-        pass
+    # def OnJobStarted(self, job):
+    #     eventType = self.GetConfigEntryWithDefault( "SetNukePluginPath", "On Job Started" )
+    #     if 'Nuke' in job.PluginName and ( eventType == "On Job Started" ):
+    #         myDir = r"//lax04rend/Nikola/library/scripts/nuke"
+    #         print (myDir)
+    #         job.JobCustomPluginDirectory=myDir
+        
+    #     RepositoryUtils.SaveJob(job)      
+    #     pass
         
     def OnJobSubmitted(self, job):
-        self.CustomPluginDirectory='//lax04rend/Nikola/library/scripts/nuke'
-        # eventType = self.GetConfigEntryWithDefault( "SetNukePluginPathProperty", "On Job Started" )
-        # if 'Nuke' in job.PluginName and ( eventType == "On Job Started" or eventType == "On Job Started and On Job Finished" ):
-        #     self.CustomPluginDirectory='//lax04rend/Nikola/library/scripts/nuke'
-        #     #self.RemoveFiles( job )
+        eventType = self.GetConfigEntryWithDefault( "SetNukePluginPath", "On Job Submitted" )
+        if 'Nuke' in job.PluginName and ( eventType == "On Job Submitted" ):
+            myDir = r"\\lax04rend\Nikola\library\scripts\nuke"
+            print (myDir)
+            job.JobCustomPluginDirectory=myDir
+        
+        RepositoryUtils.SaveJob(job)
         pass
         
